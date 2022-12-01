b515838e8b562331e0a73caca802f807ea409834
Hevel (Axel) <hevelmc@gmail.com>
2022-12-01 12:34:15 +0100

    Added no upcoming event/exam pictures

--- src/components/Events.vue
+++ src/components/Events.vue
@@ -2,18 +2,26 @@
     <div class="grid grid-cols-2">
         <div class="flex flex-col h-full mr-8">
             <p class="text-white font font-thin text-[30px] mb-4">UPCOMING EVENTS</p>
-            <div class="bg-dark-background flex flex-col gap-3 p-2 w-[500px] rounded-xl">
-                <EventVue v-for="event in events[0]" :key="event.id" :name="event.name" :begin="new Date(event.begin)"
+            <div class="bg-dark-background h-full flex flex-col gap-3 p-2 w-[500px] rounded-xl">
+                <EventVue v-if="(events[0].length > 0)" v-for="event in events[0]" :key="event.id" :name="event.name" :begin="new Date(event.begin)"
                     :type="event.type" :location="event.location" :color="event.color" :duration="event.duration" :reserved="event.reserved" :max="event.max" />
-                <div v-for="n in (5 - events[0].length)" class="bg-background h-[72px] rounded-xl" />
+                <div v-if="(events[0].length > 0)" v-for="n in (5 - events[0].length)" class="bg-background h-[72px] rounded-xl" />
+                <div class="my-auto py-8" v-if="(events[0].length == 0)">
+                    <img class="m-auto" src="assets/no_event.png" />
+                    <p class="font text-[30px] text-center text-white mt-8 mx-16">There is no event scheduled for now</p>
+                </div>
             </div>
         </div>
         <div class="flex flex-col h-full ml-8">
             <p class="text-white font font-thin text-[30px] mb-4">UPCOMING EXAMS</p>
-            <div class="bg-dark-background flex flex-col gap-3 p-2 w-[500px] rounded-xl">
-                <EventVue v-for="event in events[1]" :key="event.id" :name="event.name" :begin="new Date(event.begin)"
+            <div class="bg-dark-background h-full flex flex-col gap-3 p-2 w-[500px] rounded-xl">
+                <EventVue v-if="(events[1].length > 0)" v-for="event in events[1]" :key="event.id" :name="event.name" :begin="new Date(event.begin)"
                     :type="event.type" :location="event.location" :color="event.color" :duration="event.duration" :reserved="event.reserved" :max="event.max"/>
-                <div v-for="n in (5 - events[1].length)" class="bg-background h-[72px] rounded-xl" />
+                <div v-if="(events[1].length > 0)" v-for="n in (5 - events[1].length)" class="bg-background h-[72px] rounded-xl" />
+                <div class="my-auto py-8" v-if="(events[1].length == 0)">
+                    <img class="m-auto" src="assets/no_exam.png" />
+                    <p class="font text-[30px] text-center text-white mt-8 mx-16">There is no exam scheduled for now</p>
+                </div>
             </div>
         </div>
     </div>
